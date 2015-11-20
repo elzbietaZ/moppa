e0b86016c9988f6bc248a5f59863dfae1368914d
Komp <lia.gukasyan@gmail.com>
2015-11-20 09:52:48 +0100

    Added new tests for Mobile API

--- MoppaAPI/src/main/java/org/moppa/MoppaAPI/MobileAPI/MobileHandler.java
+++ MoppaAPI/src/main/java/org/moppa/MoppaAPI/MobileAPI/MobileHandler.java
@@ -6,12 +6,15 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
 import com.wordnik.swagger.annotations.ApiResponse;
 import com.wordnik.swagger.annotations.ApiResponses;
 
@@ -23,40 +26,38 @@ public class MobileHandler {
   @Produces(MediaType.APPLICATION_JSON)
   @Path("retrieveTask")
   @ApiOperation(value = "Retrieve the task")
-  @ApiResponses(value = { @ApiResponse(code = 200, message = "OK"),
-      @ApiResponse(code = 500, message = "Something wrong in server") })
-  public Response retrieveTask() {
-
-    // example response
-    JsonObject value = Json.createObjectBuilder().add("taskId", "100500")
-        .add("status", "in progress").build();
-    return Response.status(200).entity(value).build();
+  @ApiResponses(value = { @ApiResponse(code = 200, message = "OK, the task was got"),
+      @ApiResponse(code = 400, message = "Invalid value of deviceId")})
+  public Response retrieveTask(@ApiParam(value = "Id of device") @QueryParam("deviceId") long deviceId) {
+    //deviceId should be a positive number
+	if(deviceId <= 0) {
+		return Response.status(400).build();
+	} else {
+		JsonObject value = Json.createObjectBuilder().add("taskId", "1234").add("nValue", 5)
+		        .add("deviceId", 100500).add("status", "in progress").build();
+		return Response.status(200).entity(value).build();
+	}
   }
   
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  @Path("test")
-  @ApiOperation(value = "Retrieve the task")
-  @ApiResponses(value = { @ApiResponse(code = 200, message = "OK"),
-      @ApiResponse(code = 500, message = "Something wrong in server") })
-  public Response test() {
-    return Response.status(200).entity("ok").build();
-  }
-  
-  //http://stackoverflow.com/questions/20773123/jersey-test-framework-2-5-test-post-method
-  //Task(null, 5)
   @POST
-  @Path("saveResultTask")
+  @Path("saveResultTask/{deviceId}")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   @ApiOperation(value = "Save result of the task after calculation")
   @ApiResponses(value = { @ApiResponse(code = 200, message = "OK, result was got"),
-      @ApiResponse(code = 500, message = "Something wrong in the server") })
-  public Response saveResultTask(String result) {
-
-    // example response
-    JsonObject value = Json.createObjectBuilder().add("result", "120").build();
-    return Response.status(200).entity(value).build();
+      @ApiResponse(code = 500, message = "Saving was failed") })
+  public Response saveResultTask(
+		  @ApiParam(value = "Id of device") @PathParam("deviceId") long deviceId,
+		  @ApiParam(value = "Result of task") String result) {
+	System.out.println("deviceId " + deviceId);
+	System.out.println("result " + result);
+	if ( !result.equals("") && deviceId > 0 ) {
+	    // example response
+	    JsonObject value = Json.createObjectBuilder().add("result", "120").add("deviceId", 100500).build();
+	    return Response.status(200).entity(value).build();
+	} else {
+		return Response.status(400).build();
+	}
   }
 
 }
--- MoppaAPI/src/test/java/org/moppa/MoppaAPI/MobileHandlerTest.java
+++ MoppaAPI/src/test/java/org/moppa/MoppaAPI/MobileHandlerTest.java
@@ -2,25 +2,75 @@ package org.moppa.MoppaAPI;
 
 import static org.junit.Assert.*;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Assert;
 import org.junit.Test;
 import org.moppa.MoppaAPI.MobileAPI.MobileHandler;
+import org.moppa.MoppaCore.model.Task;
 
 public class MobileHandlerTest extends JerseyTest {
+	
+	MobileHandler testedClass = new MobileHandler();
+	public static final int correctNValue = 5;
+	public static final long correctDeviceId = 100500;
+	public static final long incorrectDeviceId = -100500;
+	public static final String correctResult = "12312321321321";
+	public static final String incorrectResult = "";
+	public static final Task correctTask = new Task(12, correctNValue);
+	
 	@Override
     protected Application configure() {
         return new ResourceConfig(MobileHandler.class);
     }
  
     @Test
-    public void test() {
-        final Response response = target("v1/tasks/test").request().get();
-        System.out.println(response);
+    public void retrieveTaskWithValidDeviceId() {
+        final Response response = target("v1/tasks/retrieveTask")
+        		.queryParam("deviceId", 100500)
+        		.request(MediaType.APPLICATION_JSON_TYPE)
+        		.get();
         assertEquals(response.getStatus(), 200);
-        assertEquals(response.readEntity(String.class), "ok");
+    }
+    
+    @Test
+    public void retrieveTaskWithInvalidDeviceId() {
+        final Response response = target("v1/tasks/retrieveTask")
+        		.request(MediaType.APPLICATION_JSON_TYPE)
+        		.get();
+        assertEquals(response.getStatus(), 400);
+    }
+    
+    @Test
+    public void saveResultTaskSuccessfully() {
+    	Entity<String> resultEntity = Entity.entity(correctResult, MediaType.APPLICATION_JSON);
+    	Response response = target("v1/tasks/saveResultTask/" + correctDeviceId).request().post(resultEntity);
+        Assert.assertEquals(200, response.getStatus());
+    }
+    
+    @Test
+    public void saveResultTaskFailed() {
+		Entity<String> resultEntity = Entity.entity(correctResult, MediaType.APPLICATION_JSON);
+		Response response = target("v1/tasks/saveResultTask/" + incorrectDeviceId).request().post(resultEntity);
+		Assert.assertEquals(400, response.getStatus());
+    }
+    
+    @Test
+    public void saveResultTaskFailedBecauseResultEmpty() {
+		Entity<String> resultEntity = Entity.entity(incorrectResult, MediaType.APPLICATION_JSON);
+		Response response = target("v1/tasks/saveResultTask/" + correctDeviceId).request().post(resultEntity);
+		Assert.assertEquals(400, response.getStatus());
+    }
+    
+    @Test
+    public void saveResultTaskFailedBecauseDeviceIdIncorrect() {
+		Entity<String> resultEntity = Entity.entity(incorrectResult, MediaType.APPLICATION_JSON);
+		Response response = target("v1/tasks/saveResultTask/" + incorrectDeviceId).request().post(resultEntity);
+		Assert.assertEquals(400, response.getStatus());
     }
 }
